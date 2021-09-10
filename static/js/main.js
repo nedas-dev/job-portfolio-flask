aba270d5384f26f45c71e67144a9e4d1534e1d93
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-09-10 09:39:13 -0700

    exit menu updated

--- static/js/main.js
+++ static/js/main.js
@@ -3,19 +3,31 @@ const hamburgerButton = document.querySelector('#map')
 // For nav hamburger functionality
 $(document).ready(function () {
     $('.nav-hamburger').click(function () {
+        function hideMenu(e){
+            $('.menu-content').css('display', 'none');
+            $('div.blur').css('display', 'none');
+            $('.line1').css('transform', 'rotate(0deg) translate(0px, 0px)');
+            $('.line2').css('display', 'block');
+            $('.line3').css('transform', 'translate(0px, 0px) rotate(0deg)');
+            document.removeEventListener('click', hideMenu)
+            document.querySelector('div.menu-content').removeEventListener('click', e => {
+                e.stopPropagation()
+            })
+        }
+
         if ($('.menu-content').css('display') == 'none') {
             $('.menu-content').css('display', 'block');
             $('div.blur').css('display', 'block');
             $('.line1').css('transform', 'translate(0px, 11px) rotate(45deg)');
             $('.line2').css('display', 'none');
             $('.line3').css('transform', 'translate(0px, -4px) rotate(-45deg)');
-        } else {
-            $('.menu-content').css('display', 'none');
-            $('div.blur').css('display', 'none');
-            $('.line1').css('transform', 'rotate(0deg) translate(0px, 0px)');
-            $('.line2').css('display', 'block');
-            $('.line3').css('transform', 'translate(0px, 0px) rotate(0deg)');
-        };
+            setTimeout(() => {
+                document.addEventListener('click', hideMenu)
+                document.querySelector('div.menu-content').addEventListener('click', e => {
+                    e.stopPropagation()
+                })
+            }, 0)
+        } 
     });
 
     $('.location').click(function () {
